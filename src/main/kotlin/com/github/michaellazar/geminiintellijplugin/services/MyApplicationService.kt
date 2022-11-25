1b7bde2335947bc184f6fcf52138672244cade23
GitHub Action <action@github.com>
2022-11-25 21:17:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/michaellazar/geminiintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/michaellazar/geminiintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.michaellazar.geminiintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.michaellazar.geminiintellijplugin.MyBundle
 
 class MyApplicationService {
 
